a238dd7f852a022d0e2e7c7ea4bc6262ca1e9f22
Joakim Reinert <mail@jreinert.com>
2014-03-18 19:38:10 +0100

    Initial commit

---
+++ lib/rpanel.rb
@@ -0,0 +1,10 @@
+require "rpanel/version"
+
+# A library providing a DSL to easily define and configure panels
+# i.e. to use with Dzen2
+#
+# @author Joakim Reinert
+
+module Rpanel
+  # Your code goes here...
+end
---
+++ lib/rpanel/version.rb
@@ -0,0 +1,6 @@
+module Rpanel
+
+  # Current version of Rpanel
+
+  VERSION = "0.0.1"
+end
